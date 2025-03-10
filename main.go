82c5e401f04afed1b8c623cfdbaa451b1b795efa
mikabrytu <mikabrytu@gmail.com>
2025-03-10 14:39:34 -0300

    Fyne demo on main.go

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
 	fmt.Println("Hello Fyne")
+
+	a := app.New()
+	window := a.NewWindow("Test Fyne")
+	label := widget.NewLabel("Hello Fyne!")
+	button := widget.NewButton("Hi!", func() {
+		fmt.Println("Button Pressed!")
+	})
+
+	window.SetContent(
+		container.NewVBox(label, button),
+	)
+	window.ShowAndRun()
 }

a8873483eb66f3ead89ef9a14373dce6aa083242
Mikabrytu <mikabrytu@gmail.com>
2025-03-11 01:33:44 -0300

    [SCREEN] Add SDL2 binding package (#2)

--- main.go
+++ main.go
@@ -3,23 +3,46 @@ package main
 import (
 	"fmt"
 
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
-	fmt.Println("Hello Fyne")
+	fmt.Println("Hello Gomes!")
 
-	a := app.New()
-	window := a.NewWindow("Test Fyne")
-	label := widget.NewLabel("Hello Fyne!")
-	button := widget.NewButton("Hi!", func() {
-		fmt.Println("Button Pressed!")
-	})
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
 
-	window.SetContent(
-		container.NewVBox(label, button),
-	)
-	window.ShowAndRun()
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(nil, 0)
+
+	rect := sdl.Rect{0, 0, 200, 200}
+	colour := sdl.Color{R: 255, G: 0, B: 255, A: 255} // purple
+	pixel := sdl.MapRGBA(surface.Format, colour.R, colour.G, colour.B, colour.A)
+	surface.FillRect(&rect, pixel)
+	window.UpdateSurface()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent: // NOTE: Please use `*sdl.QuitEvent` for `v0.4.x` (current version).
+				println("Quit")
+				running = false
+				break
+			}
+		}
+
+		sdl.Delay(33)
+	}
 }

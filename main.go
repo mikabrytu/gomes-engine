d922f8bec6bd19032030735c1e138460491672b4
mikabrytu <mikabrytu@gmail.com>
2025-03-11 01:06:13 -0300

    Remove Fyne from project

--- main.go
+++ main.go
@@ -2,24 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	fmt.Println("Hello Fyne")
-
-	a := app.New()
-	window := a.NewWindow("Test Fyne")
-	label := widget.NewLabel("Hello Fyne!")
-	button := widget.NewButton("Hi!", func() {
-		fmt.Println("Button Pressed!")
-	})
-
-	window.SetContent(
-		container.NewVBox(label, button),
-	)
-	window.ShowAndRun()
+	fmt.Println("Hello Gomes!")
 }

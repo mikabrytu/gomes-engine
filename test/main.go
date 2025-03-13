278ed7f6a7e8a4520e6193a86b7fa69900d16b02
mikabrytu <mikabrytu@gmail.com>
2025-03-13 13:10:36 -0300

    Add test cases for each module

--- test/main.go
+++ test/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	gomesengine "github.com/mikabrytu/gomes-engine"
+	"github.com/mikabrytu/gomes-engine/lifecycle"
 	"github.com/mikabrytu/gomes-engine/screen"
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -9,6 +13,11 @@ import (
 func main() {
 	gomesengine.HiGomes()
 
+	//testScreen()
+	testLifecycle()
+}
+
+func testScreen() {
 	specs := screen.Specs{
 		Title:  "Gomes Demo",
 		Posx:   sdl.WINDOWPOS_UNDEFINED,
@@ -17,4 +26,32 @@ func main() {
 		Height: 600,
 	}
 	screen.CreateScreen(specs)
+}
+
+func testLifecycle() {
+	l1 := lifecycle.Loopable{
+		Update: func() {
+			fmt.Println("Item 1 is in the Loop")
+		},
+	}
+
+	l2 := lifecycle.Loopable{
+		Update: func() {
+			fmt.Println("Item 2 is in the Loop")
+		},
+	}
+
+	lifecycle.Init()
+	l1 = lifecycle.Register(l1)
+	l2 = lifecycle.Register(l2)
+
+	go lifecycle.Run()
+
+	time.Sleep(2 * time.Second)
+	lifecycle.Stop(l1)
+
+	time.Sleep(2 * time.Second)
+	lifecycle.Stop(l2)
+
+	select {}
 }

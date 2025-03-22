749bed40a527f7aa4dcda2ea1477ce23da1fa8e3
mikabrytu <mikabrytu@gmail.com>
2025-03-22 04:29:03 -0300

    Add usage of lifecycle layer on main layer

--- gomes.go
+++ gomes.go
@@ -2,19 +2,20 @@ package gomesengine
 
 import (
 	"fmt"
-	"time"
 
+	"github.com/mikabrytu/gomes-engine/lifecycle"
 	"github.com/mikabrytu/gomes-engine/screen"
 )
 
 var specs screen.Specs
-var running bool
 
 func HiGomes() {
 	fmt.Println("Hi Gomes!")
 }
 
 func Init() {
+	lifecycle.Init()
+
 	specs = screen.Specs{
 		Title:  "Gomes Engine",
 		Posx:   0,
@@ -23,21 +24,12 @@ func Init() {
 		Height: 600,
 	}
 	screen.CreateScreen(specs)
+	lifecycle.Register(lifecycle.Loopable{
+		Update:  screen.Render,
+		Destroy: screen.Destroy,
+	})
 }
 
 func Run() {
-	running = true
-
-	go Destroy()
-
-	for running {
-		screen.Render()
-	}
-
-	screen.Destroy()
-}
-
-func Destroy() {
-	time.Sleep(5 * time.Second)
-	running = false
+	lifecycle.Run()
 }

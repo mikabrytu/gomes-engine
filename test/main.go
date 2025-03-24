3d9a779e79a16f3fcc1d3855281cb5d601465201
mikabrytu <mikabrytu@gmail.com>
2025-03-24 10:22:01 -0300

    Add genius template to test file

--- test/main.go
+++ test/main.go
@@ -1,11 +1,55 @@
 package main
 
 import (
+	"fmt"
+
 	gomesengine "github.com/mikabrytu/gomes-engine"
+	"github.com/mikabrytu/gomes-engine/lifecycle"
+	"github.com/mikabrytu/gomes-engine/render"
 )
 
 func main() {
 	gomesengine.HiGomes()
-	gomesengine.Init()
+	gomesengine.Init("Genius", 430, 430)
+
+	genius()
+
 	gomesengine.Run()
+}
+
+func genius() {
+	var size int32 = 200
+	var offset int32 = 10
+
+	rect := render.RectSpecs{
+		PosX:   offset,
+		PosY:   offset,
+		Width:  size,
+		Height: size,
+	}
+	redRect := rect
+	greenRect := rect
+	blueRect := rect
+	yellowRect := rect
+
+	greenRect.PosX += size + offset
+	blueRect.PosY += size + offset
+	yellowRect.PosX += size + offset
+	yellowRect.PosY += size + offset
+
+	drawRect(redRect, render.Red, "RED")
+	drawRect(greenRect, render.Green, "GREEN")
+	drawRect(blueRect, render.Blue, "BLUE")
+	drawRect(yellowRect, render.Yellow, "YELLOW")
+}
+
+func drawRect(rect render.RectSpecs, color render.Color, message string) {
+	lifecycle.Register(lifecycle.Loopable{
+		Init: func() {
+			fmt.Printf("Initializing %v\n", message)
+		},
+		Update: func() {
+			render.DrawSimpleShapes(rect, color)
+		},
+	})
 }

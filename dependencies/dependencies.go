31e3d2aa39b0a5c5940805d7a3f06e415dc111ff
mikabrytu <mikabrytu@gmail.com>
2025-04-13 00:23:36 -0300

    Init mix package and open audio to be used

--- dependencies/dependencies.go
+++ dependencies/dependencies.go
@@ -1,13 +1,31 @@
 package dependencies
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"github.com/veandco/go-sdl2/mix"
+	"github.com/veandco/go-sdl2/sdl"
+)
 
 func Init() {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
+
+	if err := mix.Init(mix.INIT_OGG); err != nil {
+		panic(err)
+	}
+
+	if err := mix.OpenAudio(
+		mix.DEFAULT_FREQUENCY,
+		mix.DEFAULT_FORMAT,
+		mix.DEFAULT_CHANNELS,
+		mix.DEFAULT_CHUNKSIZE,
+	); err != nil {
+		panic(err)
+	}
 }
 
 func Quit() {
+	mix.CloseAudio()
+	mix.Quit()
 	sdl.Quit()
 }

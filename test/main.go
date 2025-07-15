f9bd5e6ab025e28be305c444ef02c43eb6d6eeea
Mikabrytu <mikabrytu@gmail.com>
2025-07-15 19:37:58 -0300

    [SYSTEMS] Add Save System (#25)
    * Open json file
    
    * Read json values and use it on code
    
    * Serialize data from go to json and update test case
    
    * Write json data to file
    
    * Update test case

--- test/main.go
+++ test/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	gomesengine "github.com/mikabrytu/gomes-engine"
-	"github.com/mikabrytu/gomes-engine/events"
+	savesystem "github.com/mikabrytu/gomes-engine/systems/save"
+
 	"github.com/mikabrytu/gomes-engine/lifecycle"
 	"github.com/mikabrytu/gomes-engine/math"
-	"github.com/mikabrytu/gomes-engine/physics"
-	"github.com/mikabrytu/gomes-engine/render"
-	"github.com/mikabrytu/gomes-engine/ui"
-	"github.com/mikabrytu/gomes-engine/utils"
 )
 
 var SCREEN_SIZE = math.Vector2{
@@ -20,150 +18,40 @@ var SCREEN_SIZE = math.Vector2{
 
 func main() {
 	gomesengine.HiGomes()
-	gomesengine.Init("Gong", int32(SCREEN_SIZE.X), int32(SCREEN_SIZE.Y))
+	gomesengine.Init("Save System", int32(SCREEN_SIZE.X), int32(SCREEN_SIZE.Y))
+
 	lifecycle.SetSmoothStep(0.9)
 
-	gong()
+	//save()
+	//load()
 
 	gomesengine.Run()
 }
 
-func gong() {
-	const pw int = 50
-	const ph int = 200
-	const off int = 10
-
-	preparePallets(off, pw, ph)
-	prepareBall(pw)
-	prepareText()
+type Score struct {
+	Score     int       `json:"score"`
+	Timestamp time.Time `json:"timestamp"`
 }
 
-func preparePallets(off, pw, ph int) {
-	palletLeft := utils.RectSpecs{
-		PosX:   off,
-		PosY:   (SCREEN_SIZE.Y / 2) - (ph / 2),
-		Width:  pw,
-		Height: ph,
+func save() {
+	path := "test/assets/json/highscore.json"
+	timestamp := time.Now()
+	data := Score{
+		Score:     200,
+		Timestamp: timestamp,
 	}
-	palletRight := palletLeft
-	palletRight.PosX = (SCREEN_SIZE.X - (pw + off))
-	//palletRight.PosY -= 50
 
-	drawPallets(palletLeft, render.White, "palletLeft")
-	drawPallets(palletRight, render.White, "palletRight")
+	savesystem.Save(data, path)
 }
 
-func prepareBall(pw int) {
-	ball := utils.RectSpecs{
-		PosX:   (SCREEN_SIZE.X / 2) - (pw / 2),
-		PosY:   (SCREEN_SIZE.Y / 2) - (pw / 2),
-		Width:  pw,
-		Height: pw,
+func load() {
+	var score Score
+	path := "test/assets/json/highscore.json"
+	err := savesystem.Load(path, &score)
+
+	if err != nil {
+		panic(err)
 	}
-	var speed int = 10
-	var body physics.RigidBody
 
-	lifecycle.Register(lifecycle.GameObject{
-		Start: func() {
-			body = physics.RegisterBody(&ball, "ball")
-			body.Axis.X = 1
-			physics.EnableDynamicCollision(&body)
-		},
-		Physics: func() {
-			ball.PosX += speed * body.Axis.X
-			ball.PosY += speed * body.Axis.Y
-
-			physics.ResolveDynamicCollisions(&body, true, true)
-
-			if body.Rect.PosY < 0 {
-				body.Axis.Y = 1
-			}
-
-			if (body.Rect.PosY + body.Rect.Height) > SCREEN_SIZE.Y {
-				body.Axis.Y = -1
-			}
-		},
-		Render: func() {
-			render.DrawSimpleShapes(ball, render.White)
-
-			// fps := lifecycle.ShowFPS()
-			// fmt.Printf("FPS: %v\n", int(fps))
-		},
-	})
-}
-
-func prepareText() {
-	font := ui.FontSpecs{
-		Name: "Sans",
-		Path: "test/assets/font/freesansbold.ttf",
-		Size: 24,
-	}
-	position := math.Vector2{X: 10, Y: 10}
-	offset := math.Vector2{X: -64, Y: 10}
-
-	p1Score := ui.NewFont(font, SCREEN_SIZE)
-	p1Score.Init("Player 1", render.White, position)
-	p1Score.AlignText(ui.BottomCenter, offset)
-
-	position.Y = SCREEN_SIZE.Y - 10
-	p2Score := ui.NewFont(font, SCREEN_SIZE)
-	p2Score.Init("Player 2", render.White, position)
-	p2Score.AlignText(ui.TopCenter, offset)
-
-	count := 0
-	events.Subscribe(events.INPUT_MOUSE_CLICK, func(params ...any) error {
-		count++
-		text := "Player " + fmt.Sprint(count)
-		p1Score.UpdateText(text)
-
-		fmt.Printf("Updated Count: %v\n", count)
-
-		return nil
-	})
-}
-
-func drawPallets(rect utils.RectSpecs, color render.Color, name string) {
-	var body physics.RigidBody
-	var instance lifecycle.GameObject
-	speed := 5
-
-	instance = lifecycle.Register(lifecycle.GameObject{
-		Start: func() {
-			body = physics.RegisterBody(&rect, name)
-
-			events.Subscribe(events.INPUT_KEYBOARD_PRESSED_W, func(params ...any) error {
-				body.Axis.Y = -1
-				return nil
-			})
-
-			events.Subscribe(events.INPUT_KEYBOARD_PRESSED_S, func(params ...any) error {
-				body.Axis.Y = 1
-				return nil
-			})
-
-			events.Subscribe(events.INPUT_KEYBOARD_RELEASED_W, func(params ...any) error {
-				body.Axis.Y = 0
-				return nil
-			})
-
-			events.Subscribe(events.INPUT_KEYBOARD_RELEASED_S, func(params ...any) error {
-				body.Axis.Y = 0
-				return nil
-			})
-		},
-		Physics: func() {
-			body.Rect.PosY += body.Axis.Y * speed
-
-			collision := physics.CheckCollision(&body)
-			if collision.Name != "nil" {
-				lifecycle.Stop(instance)
-			}
-		},
-		Render: func() {
-			render.DrawSimpleShapes(rect, color)
-		},
-		Destroy: func() {
-			physics.RemoveBody(&body)
-		},
-	})
+	fmt.Printf("Score of %v saved at %v", score.Score, score.Timestamp)
 }

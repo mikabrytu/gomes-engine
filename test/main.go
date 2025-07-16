856d4711b276d9446516d58562234c2af64254ea
mikabrytu <mikabrytu@gmail.com>
2025-07-15 21:45:33 -0300

    Update test case

--- test/main.go
+++ test/main.go
@@ -5,7 +5,10 @@ import (
 	"time"
 
 	gomesengine "github.com/mikabrytu/gomes-engine"
+	"github.com/mikabrytu/gomes-engine/events"
+	"github.com/mikabrytu/gomes-engine/render"
 	savesystem "github.com/mikabrytu/gomes-engine/systems/save"
+	"github.com/mikabrytu/gomes-engine/utils"
 
 	"github.com/mikabrytu/gomes-engine/lifecycle"
 	"github.com/mikabrytu/gomes-engine/math"
@@ -22,8 +25,7 @@ func main() {
 
 	lifecycle.SetSmoothStep(0.9)
 
-	//save()
-	//load()
+	instantiate()
 
 	gomesengine.Run()
 }
@@ -31,6 +33,35 @@ func main() {
 type Score struct {
 	Score     int       `json:"score"`
 	Timestamp time.Time `json:"timestamp"`
+}
+
+func instantiate() {
+	events.Subscribe(events.INPUT_MOUSE_CLICK_DOWN, func(params ...any) error {
+		pos := math.Vector2{
+			X: params[0].([]any)[0].([]any)[0].(int),
+			Y: params[0].([]any)[0].([]any)[1].(int),
+		}
+
+		fmt.Printf("%v\n", pos)
+
+		rect := utils.RectSpecs{
+			Width:  50,
+			Height: 50,
+			PosX:   pos.X,
+			PosY:   pos.Y,
+		}
+
+		lifecycle.Register(&lifecycle.GameObject{
+			Start: func() {
+				println("Cube started!")
+			},
+			Render: func() {
+				render.DrawSimpleShapes(rect, render.White)
+			},
+		})
+
+		return nil
+	})
 }
 
 func save() {

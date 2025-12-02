c746ceb8027b773883bda7efae6cae5596fc47eb
mikabrytu <mikabrytu@gmail.com>
2025-12-02 11:30:18 -0300

    Abstract event bus used based on layers

--- events/events.go
+++ events/events.go
@@ -32,13 +32,47 @@ func Init() {
 }
 
 func Emit(layer EventLayer, event any) {
-	go func() {
-		inputBus.Publish(event.(eventbus.Event))
-	}()
+	switch layer {
+	case Audio:
+		publishAsync(audioBus, event.(eventbus.Event))
+	case Game:
+		publishAsync(gameBus, event.(eventbus.Event))
+	case Input:
+		publishAsync(inputBus, event.(eventbus.Event))
+	case Lifecycle:
+		publishAsync(lifecycleBus, event.(eventbus.Event))
+	case Physics:
+		publishAsync(physicsBus, event.(eventbus.Event))
+	case Render:
+		publishAsync(renderBus, event.(eventbus.Event))
+	}
 }
 
 func Subscribe(layer EventLayer, key string, callback func(data any)) {
-	inputBus.Subscribe(eventbus.EventType(key), func(e eventbus.Event) {
+	switch layer {
+	case Audio:
+		subscribe(audioBus, eventbus.EventType(key), callback)
+	case Game:
+		subscribe(gameBus, eventbus.EventType(key), callback)
+	case Input:
+		subscribe(inputBus, eventbus.EventType(key), callback)
+	case Lifecycle:
+		subscribe(lifecycleBus, eventbus.EventType(key), callback)
+	case Physics:
+		subscribe(physicsBus, eventbus.EventType(key), callback)
+	case Render:
+		subscribe(renderBus, eventbus.EventType(key), callback)
+	}
+}
+
+func publishAsync(bus eventbus.EventBus, event eventbus.Event) {
+	go func() {
+		bus.Publish(event)
+	}()
+}
+
+func subscribe(bus eventbus.EventBus, eventType eventbus.EventType, callback func(data any)) {
+	bus.Subscribe(eventType, func(e eventbus.Event) {
 		callback(e)
 	})
 }

f36dbcaab9e04c5b371c80c3a35f0fe3f3499b1d
mikabrytu <mikabrytu@gmail.com>
2025-12-30 19:53:52 -0300

    Remove DrawCircle function and sdl2-gfx dependency

--- test/main.go
+++ test/main.go
@@ -7,8 +7,6 @@ import (
 	"github.com/mikabrytu/gomes-engine/events"
 	"github.com/mikabrytu/gomes-engine/lifecycle"
 	"github.com/mikabrytu/gomes-engine/math"
-	"github.com/mikabrytu/gomes-engine/render"
-	"github.com/mikabrytu/gomes-engine/utils"
 )
 
 var SCREEN_SIZE = math.Vector2{
@@ -27,25 +25,5 @@ func main() {
 		return nil
 	})
 
-	circle()
-
 	gomesengine.Run()
-}
-
-func circle() {
-	circle := utils.CircleSpecs{
-		PosX:   SCREEN_SIZE.X / 2,
-		PosY:   SCREEN_SIZE.Y / 2,
-		Radius: 32,
-	}
-
-	lifecycle.Register(&lifecycle.GameObject{
-		Update: func() {
-			circle.PosX += 1
-			circle.PosY += 1
-		},
-		Render: func() {
-			render.DrawCircle(circle, render.White)
-		},
-	})
 }

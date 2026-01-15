b4d5b043aba8ace2f59a5104e3189dd046b5efbb
mikabrytu <mikabrytu@gmail.com>
2026-01-14 23:50:58 -0300

    Font pipeline remade to be similar to sprite and move it to render package

---
+++ render/font.go
@@ -0,0 +1,135 @@
+package render
+
+import (
+	"github.com/mikabrytu/gomes-engine/math"
+	"github.com/mikabrytu/gomes-engine/utils"
+	"github.com/veandco/go-sdl2/ttf"
+)
+
+type FontSpecs struct {
+	Name string
+	Path string
+	Size int
+}
+
+type Anchor int
+
+const (
+	TopLeft Anchor = iota
+	TopRight
+	TopCenter
+	MiddleLeft
+	MiddleRight
+	MiddleCenter
+	BottomLeft
+	BottomRight
+	BottomCenter
+)
+
+type Font struct {
+	instance *ttf.Font
+	text     string
+	position math.Vector2
+	rect     utils.RectSpecs
+	color    Color
+	screen   math.Vector2
+	update   bool
+	enabled  bool
+}
+
+func NewFont(specs FontSpecs, screenSize math.Vector2) *Font {
+	font := &Font{
+		screen:  screenSize,
+		update:  false,
+		enabled: true,
+	}
+
+	var err error
+	font.instance, err = ttf.OpenFont(specs.Path, specs.Size)
+	if err != nil {
+		panic(err)
+	}
+
+	return font
+}
+
+func (f *Font) Init(text string, color Color, position math.Vector2) {
+	f.text = text
+	f.color = color
+	f.position = position
+
+	RegisterFont(f)
+}
+
+func (f *Font) Enable() {
+	f.enabled = true
+}
+
+func (f *Font) Disable() {
+	f.enabled = false
+}
+
+func (f *Font) IsEnable() bool {
+	return f.enabled
+}
+
+func (f *Font) UpdateText(text string) {
+	f.Reset()
+	f.Init(text, f.color, f.position)
+}
+
+func (f *Font) UpdateColor(color Color) {
+	f.color = color
+	f.update = true
+}
+
+func (f *Font) UpdatePosition(position math.Vector2) {
+	f.position = position
+	f.update = true
+}
+
+func (f *Font) AlignText(anchor Anchor, offset math.Vector2) {
+	switch anchor {
+	case TopLeft:
+		f.rect.PosX = 0 + offset.X
+		f.rect.PosY = 0 + offset.Y
+	case TopCenter:
+		f.rect.PosX = ((f.screen.X / 2) - (f.rect.Width / 2)) + offset.X
+		f.rect.PosY = 0 + offset.Y
+	case TopRight:
+		f.rect.PosX = f.screen.X - (f.rect.Width + offset.X)
+		f.rect.PosY = 0 + offset.Y
+	case MiddleLeft:
+		f.rect.PosX = 0 + offset.X
+		f.rect.PosY = ((f.screen.Y / 2) - (f.rect.Height / 2)) + offset.Y
+	case MiddleCenter:
+		f.rect.PosX = ((f.screen.X / 2) - (f.rect.Width / 2)) + offset.X
+		f.rect.PosY = ((f.screen.Y / 2) - (f.rect.Height / 2)) + offset.Y
+	case MiddleRight:
+		f.rect.PosX = f.screen.X - (f.rect.Width + offset.X)
+		f.rect.PosY = ((f.screen.Y / 2) - (f.rect.Height / 2)) + offset.Y
+	case BottomLeft:
+		f.rect.PosX = 0 + offset.X
+		f.rect.PosY = f.screen.Y - (f.rect.Height + offset.Y)
+	case BottomCenter:
+		f.rect.PosX = ((f.screen.X / 2) - (f.rect.Width / 2)) + offset.X
+		f.rect.PosY = f.screen.Y - (f.rect.Height + offset.Y)
+	case BottomRight:
+		f.rect.PosX = f.screen.X - (f.rect.Width + offset.X)
+		f.rect.PosY = f.screen.Y - (f.rect.Height + offset.Y)
+	}
+
+	f.UpdatePosition(math.Vector2{
+		X: f.rect.PosX,
+		Y: f.rect.PosY,
+	})
+}
+
+func (f *Font) Reset() {
+	ClearFont(f)
+}
+
+func (f *Font) Clear() {
+	f.instance.Close()
+	ClearFont(f)
+}
--- render/render.go
+++ render/render.go
@@ -3,6 +3,7 @@ package render
 import (
 	"github.com/mikabrytu/gomes-engine/debug"
 	"github.com/mikabrytu/gomes-engine/lifecycle"
+	"github.com/mikabrytu/gomes-engine/utils"
 	"github.com/veandco/go-sdl2/img"
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -15,18 +16,29 @@ type ScreenSpecs struct {
 	Height int32
 }
 
-type CopySpecs struct {
+type CopySprite struct {
 	sprite  *Sprite
 	texture *sdl.Texture
 	rect    *sdl.Rect
 	color   Color
 }
 
+type CopyFont struct {
+	font    *Font
+	texture *sdl.Texture
+	rect    *sdl.Rect
+}
+
 var window *sdl.Window
 var renderer *sdl.Renderer
-var toCreate []*Sprite
-var toDestroy []*Sprite
-var copies []CopySpecs
+
+var spriteCreateQueue []*Sprite
+var spriteDestroyQueue []*Sprite
+var fontCreateQueue []*Font
+var fontDestroyQueue []*Font
+var spriteCopies []CopySprite
+var fontCopies []CopyFont
+
 var backgroundColor Color
 
 func CreateScreen(s ScreenSpecs) {
@@ -42,9 +54,12 @@ func CreateScreen(s ScreenSpecs) {
 		panic(err)
 	}
 
-	toCreate = make([]*Sprite, 0)
-	toDestroy = make([]*Sprite, 0)
-	copies = make([]CopySpecs, 0)
+	spriteCreateQueue = make([]*Sprite, 0)
+	spriteDestroyQueue = make([]*Sprite, 0)
+	fontCreateQueue = make([]*Font, 0)
+	fontDestroyQueue = make([]*Font, 0)
+	spriteCopies = make([]CopySprite, 0)
+	fontCopies = make([]CopyFont, 0)
 
 	backgroundColor = Black
 }
@@ -71,14 +86,16 @@ func Render() {
 	)
 	renderer.Clear()
 
-	createTextures()
+	createSprites()
+	createFonts()
 
-	for _, copy := range copies {
+	for _, copy := range spriteCopies {
 		if copy.sprite.enabled == false {
 			continue
 		}
 
 		if copy.sprite.update {
+			copy.sprite.update = false
 			copy.rect = &sdl.Rect{
 				X: int32(copy.sprite.rect.PosX),
 				Y: int32(copy.sprite.rect.PosY),
@@ -94,17 +111,54 @@ func Render() {
 		)
 	}
 
+	for _, copy := range fontCopies {
+		if copy.font.enabled == false {
+			continue
+		}
+
+		if copy.font.update {
+			copy.font.update = false
+			copy.texture.SetColorMod(
+				copy.font.color.R,
+				copy.font.color.G,
+				copy.font.color.B,
+			)
+
+			copy.rect = &sdl.Rect{
+				X: int32(copy.font.rect.PosX),
+				Y: int32(copy.font.rect.PosY),
+				W: int32(copy.font.rect.Width),
+				H: int32(copy.font.rect.Height),
+			}
+		}
+
+		renderer.Copy(
+			copy.texture,
+			nil,
+			copy.rect,
+		)
+	}
+
 	renderer.Present()
 
-	destroyTextures()
+	destroySprites()
+	destroyFonts()
 }
 
 func RegisterSprite(sprite *Sprite) {
-	toCreate = append(toCreate, sprite)
+	spriteCreateQueue = append(spriteCreateQueue, sprite)
+}
+
+func RegisterFont(font *Font) {
+	fontCreateQueue = append(fontCreateQueue, font)
 }
 
 func ClearSprite(sprite *Sprite) {
-	toDestroy = append(toDestroy, sprite)
+	spriteDestroyQueue = append(spriteDestroyQueue, sprite)
+}
+
+func ClearFont(font *Font) {
+	fontDestroyQueue = append(fontDestroyQueue, font)
 }
 
 func GetRenderer() *sdl.Renderer {
@@ -122,18 +176,18 @@ func Destroy() {
 	lifecycle.Kill()
 }
 
-func createTextures() {
-	if len(toCreate) == 0 {
+func createSprites() {
+	if len(spriteCreateQueue) == 0 {
 		return
 	}
 
-	for _, sprite := range toCreate {
+	for _, sprite := range spriteCreateQueue {
 		texture, err := img.LoadTexture(renderer, sprite.path)
 		if err != nil {
 			panic(err)
 		}
 
-		copy := CopySpecs{
+		copy := CopySprite{
 			sprite:  sprite,
 			texture: texture,
 			rect: &sdl.Rect{
@@ -144,19 +198,67 @@ func createTextures() {
 			},
 			color: sprite.color,
 		}
-		copies = append(copies, copy)
+		spriteCopies = append(spriteCopies, copy)
 	}
 
-	toCreate = toCreate[:0]
+	spriteCreateQueue = spriteCreateQueue[:0]
 }
 
-func destroyTextures() {
-	if len(toDestroy) == 0 {
+func createFonts() {
+	if len(fontCreateQueue) == 0 {
 		return
 	}
 
-	for _, sprite := range toDestroy {
-		for i, copy := range copies {
+	for _, font := range fontCreateQueue {
+		csdl := sdl.Color{
+			R: font.color.R,
+			G: font.color.G,
+			B: font.color.B,
+			A: font.color.A,
+		}
+
+		surface, err := font.instance.RenderUTF8Blended(font.text, csdl)
+		if err != nil {
+			panic(err)
+		}
+
+		texture, err := renderer.CreateTextureFromSurface(surface)
+		if err != nil {
+			panic(err)
+		}
+
+		copy := CopyFont{
+			font:    font,
+			texture: texture,
+			rect: &sdl.Rect{
+				X: int32(font.position.X),
+				Y: int32(font.position.Y),
+				W: int32(surface.W),
+				H: int32(surface.H),
+			},
+		}
+
+		font.rect = utils.RectSpecs{
+			PosX:   int(copy.rect.X),
+			PosY:   int(copy.rect.Y),
+			Width:  int(copy.rect.W),
+			Height: int(copy.rect.H),
+		}
+		fontCopies = append(fontCopies, copy)
+
+		surface.Free()
+	}
+
+	fontCreateQueue = fontCreateQueue[:0]
+}
+
+func destroySprites() {
+	if len(spriteDestroyQueue) == 0 {
+		return
+	}
+
+	for _, sprite := range spriteDestroyQueue {
+		for i, copy := range spriteCopies {
 			if copy.sprite != sprite {
 				continue
 			}
@@ -166,10 +268,34 @@ func destroyTextures() {
 				panic(err)
 			}
 
-			copies = append(copies[:i], copies[i+1:]...)
+			spriteCopies = append(spriteCopies[:i], spriteCopies[i+1:]...)
 			break
 		}
 	}
 
-	toDestroy = toDestroy[:0]
+	spriteDestroyQueue = spriteDestroyQueue[:0]
+}
+
+func destroyFonts() {
+	if len(fontDestroyQueue) == 0 {
+		return
+	}
+
+	for _, font := range fontDestroyQueue {
+		for i, copy := range fontCopies {
+			if copy.font != font {
+				continue
+			}
+
+			err := copy.texture.Destroy()
+			if err != nil {
+				panic(err)
+			}
+
+			fontCopies = append(fontCopies[:i], fontCopies[i+1:]...)
+			break
+		}
+	}
+
+	fontDestroyQueue = fontDestroyQueue[:0]
 }
